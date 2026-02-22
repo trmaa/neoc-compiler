62bb6aa4a5109dcaeb2f9cc963735d4d27b943bc
trmaa <trmaayt@gmail.com>
2026-02-22 21:38:11 +0100

    functions

--- main.c
+++ main.c
@@ -52,7 +52,8 @@ inferr:
 
 	else if (val[0] == '\'')
 		strcpy(type, "char");
-		// TODO support array { ... } inferrence.
+		// TODO support array -> let arr = { ... }.
+		// TODO support references -> let b = &a.
 	else
 		if (val[strlen(val)-1] == 'f')
 			strcpy(type, "float");
@@ -72,6 +73,85 @@ end:
 	}
 }
 
+void parse_fn()
+{
+	char type[BUFF_LEN] = "";
+	char whole_func[BUFF_LEN * 4] = "";
+
+	char c;
+	int i = 0;
+
+	while ((c = getchar()) != EOF && c != '{') // printf doesn't seem to work in this case...
+		whole_func[i++] = c;
+
+	int len = i;
+
+	for (i = 0; i < len; i++)
+		if (whole_func[i] == '~')
+			sprintf(type, "%s", &whole_func[i+1]);
+			//sscanf(&whole_func[i], "~%s", type);
+	
+	// type is set
+	
+	type[strlen(type)-1] = 0; // eliminate a space in the end
+	
+	bool is_defined = type[strlen(type)-1] != ';';
+
+	if (!is_defined)
+		type[strlen(type)-1] = 0;
+
+	printf("%s ", type);
+
+	char name[BUFF_LEN] = "";
+
+	for (i = 0; i < len && whole_func[i] != '('; i++)
+		name[i] = whole_func[i];
+
+	i++; // now the i is the index of (
+
+	printf("%s(", name);
+
+	char var_name[BUFF_LEN] = "";
+	char var_type[BUFF_LEN] = "";
+
+	char fase = 0;
+
+	char read = 0;
+
+	for (;i < len && whole_func[i] != ')'; i++) {
+		c = whole_func[i];
+
+		if (c == ' ')
+			continue;
+
+		if (c == ':') {
+			fase = 1;
+			read = 0;
+			continue;
+		} 
+		
+		if (c == ',') {
+			fase = 0;
+			read = 0;
+
+			printf("%s %s, ", var_type, var_name);
+			continue;
+		}
+
+		switch (fase) {
+		case 0:
+			var_name[read++] = c;
+		case 1:
+			var_type[read++] = c;
+		}	
+	}
+
+	if (is_defined)
+		printf("%s %s)\n{", var_type, var_name);
+	else
+		printf("%s %s);\n", var_type, var_name);
+}
+
 int main()
 {
 	char c;
@@ -85,13 +165,21 @@ int main()
 		end_of_keyword = c == ' ' || c == '\n' || c == '\t';
 
 		// parsing
-		if (!strcmp(buff, "let")) {
+		if (!strcmp(buff, "//")) {
+			while ((c = getchar()) != EOF && c != '\n'); // ignoring
+		}
+
+		else if (!strcmp(buff, "let")) {
 			parse_let();
 		}
 
 		else if (!strcmp(buff, "const")) {
 			printf("const ");
 			parse_let();
+		}
+
+		else if (!strcmp(buff, "fn")) {
+			parse_fn();
 		}
 
 		else if (!strcmp(buff, "main")) {
---
+++ tests/func.nc
@@ -0,0 +1,5 @@
+fn sum(a: int, b: int) ~int {
+	return a + b;
+}
+
+fn sum(a: int, b: int) ~int;
